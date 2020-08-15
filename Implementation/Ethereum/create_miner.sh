b64d2f90e3bab12780134bd8821a06a43794c5a7
Tristan NEMOZ <tristan.nemoz19@imperial.ac.uk>
2020-08-15 19:01:54 +0200

    Working private Ethereum blockchain

--- Implementation/Ethereum/create_miner.sh
+++ Implementation/Ethereum/create_miner.sh
@@ -7,8 +7,10 @@ else
    echo "Please run the script using bash or zsh."
    exit 2
 fi
-mkdir -p miner
-geth --datadir miner account new --password miner
-geth --datadir miner init genesis.json
+if [ ! -d "miner" ]; then
+    mkdir  miner
+    geth --datadir miner account new
+    geth --datadir miner init genesis.json
+fi
 geth --identity miner --http --http.port 8000 --http.corsdomain "*" --datadir miner --port 30303 --nodiscover --http.api "eth,net,web3,personal,miner,admin" --networkid 1900 --nat "any"
 cd $CURDIR

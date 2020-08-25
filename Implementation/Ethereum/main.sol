700e7a27fe5c4e06f22757182d2fe0918cdf2bf4
Tristan NEMOZ <tristan.nemoz19@imperial.ac.uk>
2020-08-25 03:11:20 +0200

    Changing the protocol design

--- Implementation/Ethereum/main.sol
+++ Implementation/Ethereum/main.sol
@@ -5,20 +5,19 @@ import {BytesLib} from "https://github.com/GNSPS/solidity-bytes-utils/blob/maste
 // import {BTCUtils} from '@interlay/bitcoin-spv-sol/contracts/BTCUtils.sol';
 // import {ValidateSPV} from '@interlay/bitcoin-spv-sol/contracts/ValidateSPV.sol';
 
+// An implementation of FlyClient as a cross-chain relay. Note that this implementation assumes that FlyClient is deployed either as a hard or as a soft fork.
 contract FlyClient {
     using SafeMath for uint256;
     using BytesLib for bytes;
-
-    address manager;
     
-    modifier onlyManager() {
-        require(msg.sender == manager, "Access restricted function.");
-        _;
+    struct Commitment {
+        uint64 chainLength;
+        bytes32 mmrRoot;
+        bytes32 headerHash;
+        bytes32 txId;
     }
     
-    constructor() public {
-        manager = msg.sender;
-    }
+    mapping(address => Commitment) commitments;
     
     
     function reverseEndianness(bytes memory toReverse) public pure returns (bytes memory) {
@@ -35,23 +34,11 @@ contract FlyClient {
         return reverseEndianness(abi.encodePacked(sha256(abi.encodePacked(sha256(toHash))))).toBytes32(0);
     }
     
-    struct BlockHeader {
-        bytes32 previousBlockHeaderHash;
-        bytes32 merkleRootHash;
-        bytes32 mmrRoot;
-        bool isPowValid;
-        uint64 height;
-    }
     
-    mapping(bytes32 => BlockHeader) private blockHeaders;
-
-    // TODO: Storage isn't cheap: remember to delete the entry thereafter: delete blockHeaders[blockHeaderHash]
     // TODO: We probably want the function to be external rather than public and the data location to be calldata rather than memory
-    function parseBlockHeader(bytes memory header) public onlyManager returns (bytes32) {
+    function verifyBlockHeader(bytes memory header) public pure returns (bool) {
         assert(header.length == 80);
         bytes32 blockHeaderHash = doubleSha256(header);
-        
-        BlockHeader storage blockHeader = blockHeaders[blockHeaderHash];
         
         // Checking for PoW
         // TODO: Use toUint32 function from ByteLib for a more efficient computation
@@ -69,9 +56,11 @@ contract FlyClient {
         }
         
         target = target.mul(256 ** (uint(uint8(header[75])).sub(3)));
-        // TODO: do we have to check the timestamp?
-        blockHeader.isPowValid = pow <= target;
         
+        if (pow > target) {
+            return false;
+        }
+
         // If the version needs to be checked, uncomment the following lines
         // uint version;
         
@@ -79,95 +68,100 @@ contract FlyClient {
         //     version = version.add((uint(uint8(header[i])).mul(256 ** i)));
         // }
         
-        // require(version <= 4);
+        // if (version > 4) {
+            // return false;
+        // }
         
-        blockHeader.previousBlockHeaderHash = reverseEndianness(header.slice(4, 32)).toBytes32(0);
-        blockHeader.merkleRootHash = reverseEndianness(header.slice(36, 32)).toBytes32(0);
-
-        return blockHeaderHash;
+        // TODO: do we have to check the timestamp?
+        return true;
     }
     
-    // We only are interested by the generation transaction and its coinbase attribute
-    function parseTransactions(bytes32 blockHeaderHash, bytes memory toParse) public onlyManager returns (uint){
-        require(toParse.length >= 42, "Raw transaction too short: less than 42 bytes.");
-        // The generation transaction CompactSize script bytes can't be larger than 100
-        assert(toParse[41] <= 0x64);
-        uint8 scriptBytes = uint8(toParse[41]);
-        require(toParse.length >= 46 + scriptBytes, "Raw transaction too short: can't contain the coinbase script.");
-        BlockHeader storage blockHeader = blockHeaders[blockHeaderHash];
-        blockHeader.height = uint8(toParse[43]) + 256 * uint8(toParse[44]) + (256 ** 2) * uint8(toParse[45]);
-        bytes memory coinbase = toParse.slice(46, scriptBytes);
-        
-        // Must be at least 32 bytes long + pattern
-        if (scriptBytes >= 37) {
-            // If MMR root is included, it must be included under the form MMR(mmrRoot)
-            if (uint8(coinbase[0]) == 0x4D && uint8(coinbase[1]) == 0x4D && uint8(coinbase[2]) == 0x52 && uint8(coinbase[3]) == 0x28 && uint8(coinbase[scriptBytes - 1]) == 0x29) {
-                blockHeader.mmrRoot = coinbase.slice(4, 32).toBytes32(0);
-            }
-        }
-    }
-    
-    
-    function verifyTxInclusion(bytes32 txId, bytes32 headerHash, bytes memory merkleProof, uint64 index) public view returns (bool) {
-        require(merkleProof.length % 32 == 0, "The Merkle Proof must be a concatenation of 32 bytes-long hashes.");
+    function verifyProof(bytes32 id, bytes32 root, bytes memory proof, uint64 index) public pure returns (bool) {
+        require(proof.length % 32 == 0, "The proof must be a concatenation of 32 bytes-long hashes.");
         bytes memory addedUpHashes;
         
         if (index % 2 == 0) {
-            addedUpHashes = abi.encodePacked(sha256(merkleProof.slice(0, 32).concat(abi.encodePacked(txId))));
+            addedUpHashes = abi.encodePacked(sha256(proof.slice(0, 32).concat(abi.encodePacked(id))));
         } else {
-            addedUpHashes = abi.encodePacked(sha256(abi.encodePacked(txId).concat(merkleProof.slice(0, 32))));
+            addedUpHashes = abi.encodePacked(sha256(abi.encodePacked(id).concat(proof.slice(0, 32))));
         }
         
         uint8 sliceBeginning = 32;
         index /= 2;
         
-        while (sliceBeginning < merkleProof.length) {
+        while (sliceBeginning < proof.length) {
             if (index % 2 == 0) {
-                addedUpHashes = abi.encodePacked(sha256(merkleProof.slice(sliceBeginning, 32).concat(addedUpHashes)));
+                addedUpHashes = abi.encodePacked(sha256(proof.slice(sliceBeginning, 32).concat(addedUpHashes)));
             } else {
-                addedUpHashes = abi.encodePacked(sha256(addedUpHashes.concat(merkleProof.slice(sliceBeginning, 32))));
+                addedUpHashes = abi.encodePacked(sha256(addedUpHashes.concat(proof.slice(sliceBeginning, 32))));
             }
             sliceBeginning += 32;
             index /= 2;
         }
         
-        return addedUpHashes.equal(abi.encodePacked(blockHeaders[headerHash].merkleRootHash));
+        return addedUpHashes.equal(abi.encodePacked(root));
     }
+
     
-    function verifySubmittedBlock(bytes memory toParse, bytes memory merkleProof) public onlyManager {
+    function verifySubmittedBlock(bytes memory toParse, bytes memory merkleProof, bytes memory mmrProof, bytes32 mmrRoot) public pure returns (bool) {
         require(toParse.length >= 80, "Block hash too short to contain a header.");
         bytes memory header = toParse.slice(0, 80);
         
-        // Verifies PoW and creates associated BlockHeader
-        bytes32 blockHeaderHash = parseBlockHeader(header);
-        
-        uint8 offset;
-        
-        if (toParse[80] < 0xFD) {
-            offset = 1;
-        } else if (toParse[80] == 0xFD) {
-            require(toParse.length >= 83, "Block hash too short to contain the transactions number CompactSize with first byte 0xFD.");
-            offset = 3;
-        } else if (toParse[80] == 0xFE) {
-            require(toParse.length >= 85, "Block hash too short to contain the transactions number CompactSize with first byte 0xFE.");
-            offset = 5;
-        } else {
-            require(toParse.length >= 89, "Block hash too short to contain the transactions number CompactSize with first byte 0xFF.");
-            offset = 9;
+        // Verifies Block header (PoW, ...)
+        if (!verifyBlockHeader(header)) {
+            return false;
         }
         
-        uint8 startingPoint = 80 + offset;
-        bytes memory generationTransaction = toParse.slice(startingPoint, toParse.length - startingPoint);
-        parseTransactions(blockHeaderHash, generationTransaction);
+        bytes memory generationTransaction;
+        
+        if (toParse[80] < 0xFD) {
+            generationTransaction = toParse.slice(81, toParse.length - 81);
+        } else if (toParse[80] == 0xFD) {
+            require(toParse.length >= 83, "Block hash too short to contain the transactions number CompactSize with first byte 0xFD.");
+            generationTransaction = toParse.slice(83, toParse.length - 83);
+        } else if (toParse[80] == 0xFE) {
+            require(toParse.length >= 85, "Block hash too short to contain the transactions number CompactSize with first byte 0xFE.");
+            generationTransaction = toParse.slice(85, toParse.length - 85);
+        } else {
+            require(toParse.length >= 89, "Block hash too short to contain the transactions number CompactSize with first byte 0xFF.");
+            generationTransaction = toParse.slice(89, toParse.length - 89);
+        }
+        
         // TODO: do we need to check for the validity of the outputs (ie do we have to check signatures)?
-        require(verifyTxInclusion(doubleSha256(generationTransaction), blockHeaderHash, merkleProof, 0), "The generation transaction isn't included within the block.");
+        // Check that the generation transaction is included within the block
+        if (!verifyProof(doubleSha256(generationTransaction), reverseEndianness(header.slice(36, 32)).toBytes32(0), merkleProof, 0)) {
+            return false;
+        }
+        
+        require(generationTransaction.length >= 42, "Raw transaction too short: less than 42 bytes.");
+        require(toParse[41] <= 0x64, "The generation transaction CompactSize script bytes can't be larger than 100");
+        require(toParse.length >= 46 + uint8(toParse[41]), "Raw transaction too short: can't contain the coinbase script.");
+
+        // Must be at least 32 bytes long + pattern
+        require(uint8(toParse[41]) >= 37, "ScriptBytes too short to contain the MMR root and the pattern.");
+        // MMR root must be included under the form "MMR(mmrRoot)", with "MMR(" and ")" being in ASCII, and mmrRoot being the mmrRoot in bytes in BE
+        require(
+            uint8(toParse[46]) == 0x4D && uint8(toParse[47]) == 0x4D && uint8(toParse[48]) == 0x52 && uint8(toParse[49]) == 0x28 && uint8(toParse[45 + uint8(toParse[41])]) == 0x29,
+            "Pattern not found."
+            );
+
+        return verifyProof(
+            doubleSha256(header),
+            mmrRoot,
+            mmrProof,
+            uint8(toParse[43]) + 256 * uint8(toParse[44]) + (256 ** 2) * uint8(toParse[45])
+        );
     }
     
-    function setGenesis(bytes memory header) public onlyManager {
-        parseBlockHeader(header);
-    }
-    
-    function getMmrProofLength(uint64 n, uint64 k) public pure returns (uint8) {
-        assert(k <= n);
+    function commitment(
+        bytes memory containsTx,
+        bytes32 txId,
+        bytes memory merkleProofTx,
+        bytes memory merkleProofCoinbase,
+        bytes memory mmrProof,
+        uint64 chainLength,
+        bytes memory lastHeader
+    ) public {
+        
     }
 }
